281d1b6b0f8c275492ea957a29c43328651326d6
Aditya Singh <sadityakumar9211@gmail.com>
2024-04-16 13:41:07 +0530

    Revert "fix: governance token issue and add: bootstrapping scripts (#1)"
    This reverts commit f799dff80397394b7e98bdbe5f9258c3933ab878.

--- src/governance/Timelock.sol
+++ src/governance/Timelock.sol
@@ -5,7 +5,10 @@ pragma solidity ^0.8.20;
 import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
 
 contract Timelock is TimelockController {
-    constructor(uint256 minDelay, address[] memory proposers, address[] memory executors, address[] memory admins)
-        TimelockController(minDelay, proposers, executors, msg.sender)
-    {}
+    constructor(
+        uint256 minDelay,
+        address[] memory proposers,
+        address[] memory executors,
+        address[] memory admins
+    ) TimelockController(minDelay, proposers, executors, msg.sender) {}
 }
